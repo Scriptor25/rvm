d6faff180951e09505f08ae41eb1cd9e6bdff57e
Scriptor25 <f.schreiber.2006@gmail.com>
2025-09-30 12:22:24 +0200

    converted riscv-os example into gnu make project

--- riscv-os/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-riscv64-linux-gnu-as -c entry.s -o entry.o -march=rv64gc -mabi=lp64d -g
-riscv64-linux-gnu-gcc -Wall -Wextra -c -mcmodel=medany kernel.c -o kernel.o -ffreestanding -march=rv64gc -mabi=lp64d -fno-omit-frame-pointer -g
-riscv64-linux-gnu-ld -T linker.ld -nostdlib kernel.o entry.o -o kernel.elf -g
